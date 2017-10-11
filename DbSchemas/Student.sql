016d38b930220b9813da33bf4e7f18b26cdce50b
Pavel4 <reginuser@gmail.com>
2017-10-11 17:59:32 +0300

    Student and Faculty sachems were added Many-to-One

---
+++ DbSchemas/Student.sql
@@ -0,0 +1,27 @@
+﻿USE [NH_test]
+GO
+
+/****** Object:  Table [dbo].[Student]    Script Date: 10/11/2017 6:01:15 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Student](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[AcademicStanding] [int] NULL,
+	[Street] [nvarchar](100) NULL,
+	[City] [nvarchar](100) NULL,
+	[Province] [nvarchar](100) NULL,
+	[Country] [nvarchar](100) NULL,
+	[FacultyID] [int] NULL,
+ CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using CAppNH.Entities;
+using NHibernate.Cache;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
 using System;
+using System.Data;
 using System.Reflection;
 
 namespace CAppNH
@@ -28,8 +30,20 @@ namespace CAppNH
                 x.ConnectionString = "Data Source=sc0895;Initial Catalog=NH_test;Persist Security Info=True;User ID=sa;Password=@dmin2016";
                 x.Driver<SqlClientDriver>();
                 x.Dialect<MsSql2012Dialect>();
+                x.LogSqlInConsole = true;
+                x.IsolationLevel = IsolationLevel.RepeatableRead;
+                x.Timeout = 10;
+                x.BatchSize = 10;
             });
-            cfg.AddAssembly(Assembly.GetExecutingAssembly());            
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+
+            //cfg.Cache(c => {
+            //    c.UseMinimalPuts = true;
+            //    c.UseQueryCache = true;
+            //});
+            //cfg.SessionFactory().Caching.Through<HashtableCacheProvider>()
+            //   .WithDefaultExpiration(1440);
+
 
             var sefact = cfg.BuildSessionFactory();
 
@@ -37,32 +51,70 @@ namespace CAppNH
             {
                 using (var tx = session.BeginTransaction())
                 {
-                    //var newStudent = new Student() {
-                    //    FirstName = "Ivan",
-                    //    LastName = "Ivanov"
+                    var fct = session.Load<Faculty>(4);
+                    //var newFct = new Faculty()
+                    //{
+                    //    Name = "F_D"
                     //};
-                    //session.Save(newStudent);
 
-                    var students = session.CreateCriteria<Student>().List<Student>();
-
-                    foreach (var student in students)
+                    var newStudent = new Student()
                     {
-                        Console.WriteLine("{0} \t{1} \t{2} \t{3}",
-                           student.ID, student.FirstName, student.LastName, student.AcademicStanding);
-                    }
+                        FirstName = "Bobj",
+                        LastName = "Iv",
+                        Address = new Location
+                        {
+                            Street = "112 Street",
+                            City = "Minsk",
+                            Province = "M",
+                            Country = "Belarus"
+                        },
+                        Faculty = fct
+                    };
 
-                    var stdnt = session.Get<Student>(1);
+                    //fct.Students.Add(newStudent);
+
+                    //newFct.Students.Add(newStudent);
+
+                    session.Save(newStudent);
+
+                    //var students = session.CreateCriteria<Student>().List<Student>();
+                    //foreach (var student in students)
+                    //{
+                    //    Console.WriteLine("{0} \t{1} \t{2} \t{3}",
+                    //       student.ID, student.FirstName, student.LastName, student.AcademicStanding);
+                    //}
+
+                    //var stdnt = session.Get<Student>(1);
+                    //session.Delete(stdnt);
                     //Console.WriteLine("Retrieved by ID");
                     //Console.WriteLine("{0} \t{1} \t{2}", stdnt.ID, stdnt.FirstName, stdnt.LastName);
 
-                    //stdnt.AcademicStanding = StudentAcademicStanding.Good;
+
+                    //var stdnt = session.Load<Student>(1);
+
+                    //stdnt.Address = new Location
+                    //{
+                    //    Street = "123 Street",
+                    //    City = "Lahore",
+                    //    Province = "Punjab",
+                    //    Country = "Pakistan"
+                    //};
                     //session.Update(stdnt);
+
+                    //var f = new Faculty() {
+                    //    Name = "F_B"
+                    //};
+                    //session.Save(f);
+
+
+
+
 
                     tx.Commit();
                 }
 
                 Console.ReadLine();
-            }
-        }       
+            }            
+        }
     }
 }
